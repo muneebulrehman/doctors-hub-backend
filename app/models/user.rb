5e59d6e3ee16a7cd92be73e89d425ce61d028048
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-08-11 19:14:59 +0530

    Generated user schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { minimum: 6, maximum: 20 }
+  validates :email, presence: true, length: {maximum: 60}
 end
