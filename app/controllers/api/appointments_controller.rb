0891321677d3ae5b673edfeb2a80d81723397457
Didier Sangwa <50297713+sangwa7@users.noreply.github.com>
2022-08-15 13:31:45 +0200

    Add POST appointment

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -14,4 +14,36 @@ class AppointmentsController < ApplicationController
         render json: { error: appointment_error(:index) }, status: 422
       end
     end
+  
+    # GET /appointments/1
+    def show
+      render json: @appointment
+    end
+  
+    # POST /appointments
+    def create
+      params[:date] = params[:date]&.to_datetime
+  
+      @appointment = Appointment.new(appointment_params)
+  
+      if @appointment.save
+        render json: @appointment, status: :created, location: @appointment
+      else
+        render json: { error: @appointment.errors }, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    # Use callbacks to share common setup or constraints between actions.
+    def set_appointment
+      @appointment = Appointment.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: appointment_error(:show) }, status: 404
+    end
+  
+    # Only allow a list of trusted parameters through.
+    def appointment_params
+      params.permit(:doctor_id, :date, :user_id)
+    end
   end
