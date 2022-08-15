10c928ba4956f35fe2caf579021044c19d75770d
Didier Sangwa <50297713+sangwa7@users.noreply.github.com>
2022-08-15 13:09:17 +0200

    Set appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,8 @@
 class Appointment < ApplicationRecord
-end
+    validates :date, presence: true
+    validates :doctor_id, presence: true
+    validates :user_id, presence: true
+    belongs_to :user
+    belongs_to :doctor
+  end
+  
