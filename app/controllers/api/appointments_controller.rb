6f7df5950a561a01b70700059c2bdade95f18d03
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-08-15 18:31:56 +0530

    Fixed errors in appointment controller

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,5 +1,5 @@
-class AppointmentsController < ApplicationController
-  before_action :set_appointment, only: %i[show update destroy]
+class Api::AppointmentsController < ApplicationController
+  before_action :set_appointment, only: %i[show]
 
   rescue_from Date::Error, with: :invalid_date
 
@@ -15,19 +15,20 @@ class AppointmentsController < ApplicationController
     end
   end
 
-  # GET /appointments/1
+  # Get the appointment for the given id
   def show
     render json: @appointment
   end
 
-  # POST /appointments
+  # Create a new appointment
   def create
     params[:date] = params[:date]&.to_datetime
 
     @appointment = Appointment.new(appointment_params)
 
     if @appointment.save
-      render json: @appointment, status: :created, location: @appointment
+      render json: { success: true, message: 'Appointment created successfully', appointment: @appointment },
+             status: :created
     else
       render json: { error: @appointment.errors }, status: :unprocessable_entity
     end
@@ -35,15 +36,13 @@ class AppointmentsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_appointment
     @appointment = Appointment.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     render json: { error: appointment_error(:show) }, status: 404
   end
 
-  # Only allow a list of trusted parameters through.
   def appointment_params
-    params.permit(:doctor_id, :date, :user_id)
+    params.require(:appointment).permit(:doctor_id, :date, :user_id)
   end
 end
