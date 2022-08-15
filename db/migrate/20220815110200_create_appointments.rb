5eb08b1e3e1309884c5177fd5280863eaff11ea7
Didier Sangwa <50297713+sangwa7@users.noreply.github.com>
2022-08-15 13:06:57 +0200

    Set references to migrations

--- db/migrate/20220815110200_create_appointments.rb
+++ db/migrate/20220815110200_create_appointments.rb
@@ -2,6 +2,9 @@ class CreateAppointments < ActiveRecord::Migration[7.0]
   def change
     create_table :appointments do |t|
 
+      t.references :user, null: false, foreign_key: true
+      t.references :doctor, null: false, foreign_key: true
+      t.datetime : date
       t.timestamps
     end
   end
